4060aa52ce0199b609f054886c7f04763ecb1e7c
Rex Schrader <rex.schrader@gmail.com>
2020-10-17 20:09:00 +0000

    Updated Project

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 'use strict';
 
+require('dotenv').config({debug: true}); 
 const express     = require('express');
 const bodyParser  = require('body-parser');
 const expect      = require('chai').expect;
